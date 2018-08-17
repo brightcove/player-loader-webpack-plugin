93af62b2ef07a898e9b791189722cc43ec2824e7
Pat O'Neill <pgoneill@gmail.com>
2018-08-17 11:45:10 -0400

    refactor: Use @brightcove/player-url to generate URLs. (#3)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const request = require('request-promise');
 const {ConcatSource} = require('webpack-sources');
-const PlayerLoader = require('@brightcove/player-loader');
+const brightcovePlayerUrl = require('@brightcove/player-url');
 
 const defaults = {
   embedId: 'default',
@@ -9,17 +9,18 @@ const defaults = {
 };
 
 class PlayerLoaderPlugin {
+
   constructor(options) {
     const settings = Object.assign({}, defaults, options);
     const {accountId, embedId, playerId} = settings;
 
     if (!accountId || !embedId || !playerId) {
-      console.error('accountId is required and playerId/embedId must be set to a value!');
+      console.error('accountId is required and, if given, playerId and embedId must have a value!');
       console.error();
       process.exit(1);
     }
 
-    const url = PlayerLoader.getUrl(settings);
+    const url = brightcovePlayerUrl({accountId, embedId, playerId});
 
     this.playerPromise = request.get(url).catch(function(err) {
       console.error('Failed to get a player at ' + url + ' double check your options');

brightcove/player-loader-webpack-plugin

commit b5617e6347121f88e37052959a2cb3b05bed1bf2

feat: Make this plugin backwards compatible with webpack 3 (#16)

src/index.js
@@ -33,40 +33,48 @@ class PlayerLoaderPlugin {
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tapAsync('PlayerLoaderPlugin', (compilation, callback) => {
-      request.get(this.playerUrl).then((playerjs) => {
-        let assets = Object.keys(compilation.assets);
+    if (compiler.hooks && compiler.hooks.emit) {
+      // webpack@4 syntax
+      compiler.hooks.emit.tapAsync('PlayerLoaderPlugin', this.downloadAndAppendPlayer.bind(this));
+    } else {
+      // webpack@3 syntax
+      compiler.plugin('emit', this.downloadAndAppendPlayer.bind(this));
+    }
+  }
 
-        // normally we filter out all non .js outputs, and choose prepend to
-        // only the first output
-        if (typeof this.settings_.prependTo === 'undefined') {
-          // filter out non js files
-          assets = assets.filter((filename) => path.extname(filename) === '.js');
+  downloadAndAppendPlayer(compilation, callback) {
+    request.get(this.playerUrl).then((playerjs) => {
+      let assets = Object.keys(compilation.assets);
 
-          // only prepend to the first one
-          assets = [assets[0]];
+      // normally we filter out all non .js outputs, and choose prepend to
+      // only the first output
+      if (typeof this.settings_.prependTo === 'undefined') {
+        // filter out non js files
+        assets = assets.filter((filename) => path.extname(filename) === '.js');
 
-        // if prependTo is specified though, we prepend to anything that is listed
-        } else {
-          assets = assets.filter((filename) => this.settings_.prependTo.indexOf(filename) === -1);
-        }
+        // only prepend to the first one
+        assets = [assets[0]];
 
-        if (!assets.length) {
-          console.error('webpack-player-loader-plugin: did not find anything to prepend the player to!');
-          console.error();
-          process.exit(1);
-        }
+      // if prependTo is specified though, we prepend to anything that is listed
+      } else {
+        assets = assets.filter((filename) => this.settings_.prependTo.indexOf(filename) !== -1);
+      }
 
-        assets.forEach(function(file) {
-          compilation.assets[file] = new ConcatSource(playerjs, compilation.assets[file]);
-        });
-        callback();
-      }).catch(function(err) {
-        console.error('Failed to get a player at ' + this.playerUrl + ' double check your options');
-        console.error(err);
+      if (!assets.length) {
+        console.error('webpack-player-loader-plugin: did not find anything to prepend the player to!');
         console.error();
         process.exit(1);
+      }
+
+      assets.forEach(function(file) {
+        compilation.assets[file] = new ConcatSource(playerjs, compilation.assets[file]);
       });
+      callback();
+    }).catch(function(err) {
+      console.error('Failed to get a player at ' + this.playerUrl + ' double check your options');
+      console.error(err);
+      console.error();
+      process.exit(1);
     });
   }
 }

commit 23c367a4f9135bd5da06d6c86382794f8f59e035

fix: issue with introduced by prependTo from #16

src/index.js
@@ -57,7 +57,7 @@ class PlayerLoaderPlugin {
 
       // if prependTo is specified though, we prepend to anything that is listed
       } else {
-        assets = assets.filter((filename) => this.settings_.prependTo.indexOf(filename) !== -1);
+        assets = assets.filter((filename) => this.settings_.prependTo.indexOf(filename) === -1);
       }
 
       if (!assets.length) {